20739212f917392789e836db6333f6a5dfa72733
salva <salva@LAPTOP-B2KFIIUB>
2021-03-02 16:22:18 -0600

    Operadores promedios_66136.

---
+++ src/operadorepromedios_66136/OperadorePromedios_66136.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorepromedios_66136;
+
+/**
+ *
+ * @author salva
+ */
+public class OperadorePromedios_66136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    int num1 = 40;
+    int num2 = 50;
+    int num3 = 80;
+    int num4 = 10;
+    int num5 = 90;
+    
+    int promedio = ((num1 + num2 + num3 + num4 + num5)/5);
+   System.out.println("El promedio de los valores obtenidos es: " + promedio);
+     
+    }
+   
+           
+}
